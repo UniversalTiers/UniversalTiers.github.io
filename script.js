0c4daf2942553ca667fc14246819b1fb71370b78
UniversalTiers <haxxarecool@gmail.com>
2026-04-21 22:13:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getRegionName(code) {
 
 /* Hard-coded leaderboard data */
 const players = [
-  { name: "Redstone_Master", tier: "S", skin: "Redstone_Master", region: "NA" },
+  { name: "Redstone_Master", tier: "S", skin: "Gus_leme", region: "AS" },
   { name: "Coolboy2121", tier: "S", region: "EU" },
   { name: "Warsky", tier: "S", skin: "Clownpierce", region: "NA" },
   { name: "Mazes__", tier: "S", region: "AS" },
