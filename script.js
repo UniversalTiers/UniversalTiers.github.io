f9be43c5ecce799c625f040372bf5adaadd40f57
UniversalTiers <haxxarecool@gmail.com>
2026-06-16 20:34:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const players = [
   { name: "Mazes__", tier: "S", region: "AS" },
   { name: "bakedHooligan931", tier: "S", region: "AS" },
   { name: "StormyMcYt", tier: "S", region: "EU" },
+  { name: "ItzArdoks", tier: "S", region: "EU" },
   { name: "Estoniavillager", tier: "S", region: "EU" },
   { name: "BloodyA75L", tier: "S", region: "AF" },
 
